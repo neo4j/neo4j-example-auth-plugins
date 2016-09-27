b6c39ddfcc17e042718903a73d9cba73850253e2
Henrik Nyman <henrik.nyman@neotechnology.com>
2016-09-28 00:24:36 +0200

    Add project files and a first simple example

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # neo4j-example-auth-plugins
 Example authentication and authorization plugins for Neo4j
+
+To compile the code and run all tests, if you are running Java 8:
+
+    `mvn clean install`
+
+## Install plugins in Neo4j
+Copy the output jar file into the plugins folder of Neo4j Enterprise Edition 3.1 or later:
+
+    `cp plugins/target/neo4j-example-auth-plugins-<VERSION>.jar <NEO4J-HOME>/plugins/`
+
+Edit the Neo4j configuration file `<NEO4J-HOME>/conf/neo4j.conf` and add the `dbms.security.realm` setting, e.g.:
+
+    `dbms.security.realm=plugin-org.neo4j.example.auth.MyAuthPlugin`
+
+You can also enable multiple plugins simultaneously with the `dbms.security.realms` setting, e.g.:
+
+    `dbms.security.realms=plugin-org.neo4j.example.auth.MyAuthPlugin1,plugin-org.neo4j.example.auth.MyAuthPlugin2`
+
+You can also toggle authentication and authorization enabled individually, e.g.:
+
+    `dbms.security.realms.plugin.authentication_enabled=true`
+    `dbms.security.realms.plugin.authorization_enabled=false`
---
+++ plugins/src/main/java/org/neo4j/example/auth/MyAuthPlugin.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2002-2016 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.example.auth;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Properties;
+
+import org.neo4j.server.security.enterprise.auth.plugin.api.AuthToken;
+import org.neo4j.server.security.enterprise.auth.plugin.api.AuthenticationException;
+import org.neo4j.server.security.enterprise.auth.plugin.api.PredefinedRoles;
+import org.neo4j.server.security.enterprise.auth.plugin.api.RealmOperations;
+import org.neo4j.server.security.enterprise.auth.plugin.spi.AuthInfo;
+import org.neo4j.server.security.enterprise.auth.plugin.spi.AuthPlugin;
+
+public class MyAuthPlugin extends AuthPlugin.Adapter
+{
+    private RealmOperations api;
+
+    @Override
+    public AuthInfo authenticateAndAuthorize( AuthToken authToken ) throws AuthenticationException
+    {
+        String username = authToken.principal();
+        char[] password = authToken.credentials();
+
+        api.log().info( "Log in attempted for user '" + username + "'.");
+
+        if ( username != null && password != null )
+        {
+            if ( username.equals( "moraeus" ) && Arrays.equals( password, "suearom".toCharArray() ) )
+            {
+                api.log().info( "Successful log in. Welcome Kalle Moraeus! You are granted admin permissions." );
+                return AuthInfo.of( "moraeus", Collections.singleton( PredefinedRoles.ADMIN ) );
+            }
+            else if ( username.equals( "neo4j" ) && Arrays.equals( password, "neo4j".toCharArray() ) )
+            {
+                api.log().info( "Successful log in. You are granted reader permissions." );
+                return AuthInfo.of( "neo4j", Collections.singleton( PredefinedRoles.READER ) );
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void initialize( RealmOperations realmOperations )
+    {
+        api = realmOperations;
+        api.log().info( "initialized!" );
+
+        loadConfig();
+    }
+
+    private void loadConfig()
+    {
+        Path configFile = resolveConfigFilePath();
+        Properties properties = loadProperties( configFile );
+
+        String myProperty = properties.getProperty( "my.auth.property" );
+        api.log().info( "my.auth.property=" + myProperty );
+    }
+
+    private Path resolveConfigFilePath()
+    {
+        Path configFilePath;
+        Optional<Path> maybeConfigFile = api.neo4jConfigFile();
+
+        if ( maybeConfigFile.isPresent() )
+        {
+            configFilePath = maybeConfigFile.get();
+        }
+        else
+        {
+            configFilePath = api.neo4jHome().resolve( "conf/neo4j.conf" );
+        }
+        return configFilePath;
+    }
+
+    private Properties loadProperties( Path configFile )
+    {
+        Properties properties = new Properties();
+
+        try
+        {
+            InputStream inputStream = new FileInputStream( configFile.toFile() );
+            properties.load( inputStream );
+        }
+        catch ( IOException e )
+        {
+            api.log().error( "Failed to load config file '" + configFile.toString() + "'." );
+        }
+        return properties;
+    }
+}
